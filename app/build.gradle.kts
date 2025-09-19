dc91da4dcddbcbe63215c696690248a54b6950f1
张磊 <zhangl171@chinatelecom.cn>
2025-09-19 18:52:13 +0800

    发布仓库地址由 Github 修改为 MavenCentral

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,10 @@ dependencies {
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
-	implementation(project(mapOf("path" to ":GmSSL_3.1")))
-	testImplementation("junit:junit:4.13.2")
-	androidTestImplementation("androidx.test.ext:junit:1.1.5")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-	debugImplementation("androidx.compose.ui:ui-tooling")
-	debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+	if (project.findProperty("userRemote").toString().toBoolean()) {
+		implementation("io.github.heart-beats.baseproject:gmssl-android:3.1.0")
+	}else{
+		implementation(project(":gmssl-android"))
+	}
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,17 @@
 plugins {
 	id("com.android.application") version "8.1.2" apply false
 	id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+	id("com.vanniktech.maven.publish") version "0.33.0" apply false
+}
+
+apply(from = "./commonGradle/maven-publish/module_maven_publish_apply.gradle")
+
+task("clean", type = Delete::class) {
+	if (rootProject.buildDir.exists()) {
+		delete(rootProject.buildDir)
+	}
+
+	gradle.includedBuilds.forEach {
+		dependsOn(it.task(":clean"))
+	}
 }
